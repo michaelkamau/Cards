924563963bda90dafa891f864d43d56df8aba9ce
Michael Kamau <tokamau@gmail.com>
2016-10-10 12:36:39 +0300

    Add test can create an object of Card class

---
+++ test/cardtests.py
@@ -0,0 +1,11 @@
+import unittest
+from card_src.card import Card, AceCard, FaceCard, NumberCard
+
+
+class IndividualCardsTests(unittest.TestCase):
+    def test_can_create_card_object(self):
+        """
+        Should be able to create an object of Card class
+        """
+        spade_card = Card(4, 'Spade')
+        self.assertIsInstance(spade_card, Card)
