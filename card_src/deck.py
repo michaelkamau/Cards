bd96833827dadbedbeabcac425275c1ce2a976fe
Michael Kamau <tokamau@gmail.com>
2016-10-10 14:29:59 +0300

    Add definition of initilisation of Deck with 52 cards

--- card_src/deck.py
+++ card_src/deck.py
@@ -1,3 +1,18 @@
+from card_src.card import NumberCard, FaceCard, AceCard
+
+
 class Deck:
     def __init__(self):
         self.cards = []
+
+    def init_with_cards(self):
+        for rank in range(1, 14):
+            for suit in ('Clubs', 'Diamonds', 'Hearts', 'Spades'):
+                if rank == 1:
+                    self.cards.append(AceCard(rank, suit))
+                elif 2 <= rank < 11:
+                    self.cards.append(NumberCard(rank, suit))
+                elif 11 <= rank < 14:
+                    self.cards.append(FaceCard(rank, suit))
+                else:
+                    raise IndexError("Invalid rank!!")
